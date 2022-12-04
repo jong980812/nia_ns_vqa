bc86f20a54b3675926049cf63fe84f5f1845dce9
Jongseo <jong980812@khu.ac.kr>
2022-12-04 21:46:33 +0900

    First init

---
+++ scene_parse/attr_net/tools/run_train.py
@@ -0,0 +1,15 @@
+import sys
+sys.path.append("/data/jongseo/lab/ns-vqa/scene_parse/attr_net")
+from options import get_options
+from datasets import get_dataloader
+from model import get_model
+from trainer import get_trainer
+
+
+opt = get_options('train')
+train_loader = get_dataloader(opt, 'train')
+val_loader = get_dataloader(opt, 'val')
+model = get_model(opt)
+trainer = get_trainer(opt, model, train_loader, val_loader)
+
+trainer.train() 
