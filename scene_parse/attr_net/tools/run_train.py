adbeed11de4c37c3b0bd970c2c26e50a6025306d
Jongseo <jong980812@khu.ac.kr>
2022-12-05 02:38:20 +0900

    ddp verseion 1

--- scene_parse/attr_net/tools/run_train.py
+++ scene_parse/attr_net/tools/run_train.py
@@ -4,12 +4,21 @@ from options import get_options
 from datasets import get_dataloader
 from model import get_model
 from trainer import get_trainer
-
-
-opt = get_options('train')
-train_loader = get_dataloader(opt, 'train')
-val_loader = get_dataloader(opt, 'val')
-model = get_model(opt)
-trainer = get_trainer(opt, model, train_loader, val_loader)
+from ddp import launch
+def main():
+    opt = get_options('train')
+    train_loader = get_dataloader(opt, 'train')
+    val_loader = get_dataloader(opt, 'val')
+    model = get_model(opt)
+    trainer = get_trainer(opt, model, train_loader, val_loader)
 
 trainer.train() 
+if __name__ == "__main__":
+    launch(
+        main,
+        opt.num_gpus,
+        num_machines=args.num_machines,
+        machine_rank=args.machine_rank,
+        dist_url=args.dist_url,
+        args=(args,),
+    )
